f6d3e65848e31af7b63175b542dfbffedf66d29a
DahianaMC <dahiana_mc@hotmail.com>
2020-06-24 06:37:32 -0500

    Mapping single points

--- Simple_Map/Static/js/logic.js
+++ Simple_Map/Static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // // Another way to create the map object with a center and zoom level.
 // let map = L.map("mapid", {
@@ -12,23 +12,20 @@ let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 //     zoom: 4
 //   });
 
-// //  Add a marker to the map for Los Angeles, California.
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+  // We create the tile layer that will be the background of our map.
+// let streets = L.tileLayer(' https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+//     id: streets-v11,
+//     maxZoom: 18,
+//     accessToken: API_KEY
+//  });
 
-// Add a Circle to the Map, using the circle() function
-L.circle([34.0522, -118.2437], {
-  color: 'black',
-  fillColor: '#f80',
-  fillOpacity: 0.5,
-  radius: 300
-}).addTo(map);
-
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer(' https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+ let darkMap = L.tileLayer(' https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: dark-v10,
     maxZoom: 18,
     accessToken: API_KEY
  });
 
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+    // Then we add our 'graymap' tile layer to the map.
+    darkMap.addTo(map);
