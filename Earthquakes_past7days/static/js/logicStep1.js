5c5eb77fcfe46c1f029beab63e7bced8d5a92411
DahianaMC <dahiana_mc@hotmail.com>
2020-06-26 09:46:25 -0500

    adding files to Earthquakes_past7days branch

---
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -0,0 +1,42 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer(' https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'streets-v11',
+    maxZoom: 18,
+    accessToken: API_KEY
+ });
+
+ let satelliteStreets = L.tileLayer(' https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+  id: 'satellite-streets-v11',
+  maxZoom: 18,
+  accessToken: API_KEY
+});
+
+// Create the map object with center and zoom level.
+let map = L.map('mapid', {
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [streets]
+});
+
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let earthquakeData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Grabbing our GeoJSON data.
+d3.json(earthquakeData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data).addTo(map);
+});
+
+var baseMaps = {
+  'Streets': streets,
+  'Satellite': satelliteStreets
+};
+
+// Then we add our 'graymap' tile layer to the map.
+L.control.layers(baseMaps).addTo(map);
